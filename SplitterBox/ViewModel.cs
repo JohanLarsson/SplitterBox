6951eb44fd2df39853ed7a4739ced783793f1867
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-13 01:00:12 +0200

    Small sample

---
+++ SplitterBox/ViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows.Data;
+using SplitterBox.Annotations;
+
+namespace SplitterBox
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private readonly ICollectionView collectionView;
+        private string filterText;
+
+        public ViewModel()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Items.Add("isak");
+                Items.Add("reed");
+                Items.Add("dk");
+            }
+
+            collectionView = CollectionViewSource.GetDefaultView(Items);
+            collectionView.Filter = IsMatch;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FilterText
+        {
+            get { return filterText; }
+            set
+            {
+                if (value == filterText) return;
+                filterText = value;
+                OnPropertyChanged();
+                collectionView.Refresh();
+            }
+        }
+
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>();
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool IsMatch(object obj)
+        {
+            if (string.IsNullOrEmpty(FilterText))
+            {
+                return true;
+            }
+
+            var text = (string)obj;
+            return text.Contains(this.FilterText);
+        }
+    }
+}
